5c4a5195b224da8ded180ed9e3e620658242a855
Abhishek Kalhan <abhishekkalhan@Abhisheks-MacBook-Air.local>
2022-12-05 18:22:32 -0500

    Added welcome page

--- index.js
+++ index.js
@@ -31,10 +31,13 @@ mongoose.connect(database.url);
 
 var Restaurant = require("./models/restaurant");
 
+app.get("/welcome", async (req, res) => {
+  res.render("welcomePage");
+})
+
 // Register
 app.post("/register", async (req, res) => {
   // Our register logic starts here
-  console.log(req.body);
   try {
     // Get user input
     const { first_name, last_name, email, password } = req.body;
@@ -62,7 +65,6 @@ app.post("/register", async (req, res) => {
       email: email.toLowerCase(), // sanitize: convert email to lowercase
       password: encryptedPassword,
     });
-    console.log("here");
     // Create token
     const token = jwt.sign(
       { user_id: user._id, email },
@@ -77,7 +79,6 @@ app.post("/register", async (req, res) => {
     // return new user
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
   }
   // Our register logic ends here
 });
@@ -92,7 +93,6 @@ app.post("/login", async (req, res) => {
     // Validate user input
     if (!(email && password)) {
       res.status(400).send("All input is required");
-      console.log("first if");
     }
     // Validate if user exist in our database
     const user = await User.findOne({ email });
@@ -109,11 +109,9 @@ app.post("/login", async (req, res) => {
 
       // save user token
       user.token = token;
-      console.log("second if");
       // user
       res.status(200).json(user);
     } else {
-      console.log("after try");
       res.status(400).send("Invalid Credentials");
     }
   } catch (err) {
@@ -129,15 +127,11 @@ app.get("/api/restaurant/:pageCount?/:pageNum?", auth, function (req, res) {
   // use mongoose to get all todos in the database
 
   let pageCount = parseInt(req.params.pageCount);
-  console.log("pageCount: ", pageCount);
   let pageNum = parseInt(req.params.pageNum);
-  console.log("pageNum: ", pageNum);
   Restaurant.find(function (err, restaurants) {
     if (!isNaN(pageCount)) {
       let bigarray = restaurants;
-      console.log("bigarray: ", bigarray);
       var size = pageCount;
-      console.log("size: ", size);
       var arrayOfArrays = [];
       for (var i = 0; i < bigarray.length; i += size) {
         arrayOfArrays.push(bigarray.slice(i, i + size));
@@ -158,34 +152,23 @@ app.get(
   "/api/restaurant/:pageCount?/:pageNum?/:borough?",
   auth,
   function (req, res) {
-    console.log(req.body);
-
     let page = parseInt(req.params.pageCount);
-    console.log("page: ", page);
     let perPage = parseInt(req.params.pageNum);
-    console.log("perPage: ", perPage);
     let borough = req.params.borough;
-    console.log("borough: ", borough);
     let myborougharray = [];
     Restaurant.find(function (err, restaurants) {
       for (let jsondata in restaurants) {
-        //console.log(jsondata);
         if (restaurants[jsondata].borough == borough) {
-          //console.log("Here");'
           myborougharray.push(restaurants[jsondata]);
         }
       }
-      //console.log("here " + myborougharray);
       if (!isNaN(page) && !isNaN(perPage)) {
         let bigarray = myborougharray;
-        //console.log("bigarray: ", bigarray);
         var size = perPage;
-        console.log("size: ", size);
         var arrayOfArrays = [];
         for (var i = 0; i < bigarray.length; i += size) {
           arrayOfArrays.push(bigarray.slice(i, i + size));
         }
-        console.log(arrayOfArrays[page - 1]);
         if (err) res.send(err);
         res.json(arrayOfArrays[page - 1]); // return all Restaurants in JSON format
       } else {
@@ -199,44 +182,31 @@ app.get(
 );
 
 //
-app.get("/api/pageborough/:token?", async function (req, res) {
+app.get("/api/pageborough/", async function (req, res) {
   // use mongoose to get all books in the database
   res.render("allbook");
 });
 
 app.post("/api/insert/data", function (req, res) {
   // use mongoose to get all todos in the database
-
-  console.log(req.body);
-
   let page = parseInt(req.body.page);
-  console.log("page: ", page);
   let perPage = parseInt(req.body.perPage);
-  console.log("perPage: ", perPage);
   let borough = req.body.borough;
-  console.log("borough: ", borough);
   let myborougharray = [];
   Restaurant.find(function (err, restaurants) {
     for (let jsondata in restaurants) {
-      //console.log(jsondata);
       if (restaurants[jsondata].borough == borough) {
-        //console.log("Here");'
         myborougharray.push(restaurants[jsondata]);
       }
     }
-    //console.log("here " + myborougharray);
     if (!isNaN(page) && !isNaN(perPage)) {
       let bigarray = myborougharray;
-      //console.log("bigarray: ", bigarray);
       var size = perPage;
-      console.log("size: ", size);
       var arrayOfArrays = [];
       for (var i = 0; i < bigarray.length; i += size) {
         arrayOfArrays.push(bigarray.slice(i, i + size));
       }
-      console.log(arrayOfArrays[page - 1]);
       if (err) res.send(err);
-      //res.json(arrayOfArrays[page - 1]); // return all Restaurants in JSON format
       res.render("boroughdata", {
         boroughdata: arrayOfArrays[page - 1],
       });
@@ -262,10 +232,7 @@ app.get("/api/restaurantdata/:restaurant_id", auth, function (req, res) {
 // create Restaurant and send back all Restaurants after creation
 app.post("/api/restaurant", auth, function (req, res) {
   // create mongose method to create a new record into collection
-  console.log(req.body);
 
-  // let address = req.body.address;
-  // let id = req.params._id;
   let building = req.body.building;
   let coord1 = req.body.coord1;
   let coord2 = req.body.coord2;
@@ -315,7 +282,6 @@ app.post("/api/restaurant", auth, function (req, res) {
 // create Restaurant and send back all restaurant after creation
 app.put("/api/restaurant/:restaurant_id", auth, function (req, res) {
   // create mongose method to update an existing record into collection
-  console.log(req.body);
 
   let id = req.params.restaurant_id;
   var data = {
@@ -333,7 +299,6 @@ app.put("/api/restaurant/:restaurant_id", auth, function (req, res) {
 
 // delete a restaurant by id
 app.delete("/api/restaurant/:restaurant_id", auth, function (req, res) {
-  console.log(req.params.restaurant_id);
   let id = req.params.restaurant_id;
   Restaurant.remove(
     {
@@ -344,6 +309,10 @@ app.delete("/api/restaurant/:restaurant_id", auth, function (req, res) {
       else res.send("Successfully! restaurant has been Deleted.");
     }
   );
+});
+
+app.get('*', function(req, res) {
+  res.render('error', { title: 'Error', message:'Wrong Route' });
 });
 
 app.listen(port);
