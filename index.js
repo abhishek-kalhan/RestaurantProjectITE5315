41cb80bf8a4d1276c7c8426c82a4abe7d4366deb
Abhishek Kalhan <abhishekkalhan@Abhisheks-MacBook-Air.local>
2022-12-05 19:09:12 -0500

    Added declaration

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/********************************************************************************** 
+ * ITE5315 – Project* I declare that this assignment is my own work in accordance with 
+ * Humber Academic Policy.* No part of this assignment has been copied manually or electronically 
+ * from any other source* (including web sites) or distributed to other students.
+ * ** Group member Name: Abhishek Kalhan and Sagar Sharma 
+ * Student IDs: N01495445, N01481341 Date: 5/12/22
+ * *********************************************************************************/
+
 var express = require("express");
 var mongoose = require("mongoose");
 const User = require("./models/user");
